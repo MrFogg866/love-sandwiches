2bf2a24dbf9f5b857ee3e2e1428906bd1ba09289
MrFogg866 <philiplynch866@gmail.com>
2022-05-13 15:26:30 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -12,27 +13,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love-sandwiches')
 
+
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, sperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raise ValueError if strings cannot be converted into int,
-    or if there arent't exactly 6 values.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-    
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -41,6 +48,6 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-    
+        return False
 
-get_sales_data()
+    return True
